73833ec36185a19c3dd6b509a5faf6b632812175
Arafeh <arafeh198@gmail.com>
2026-04-07 20:43:17 -0400

    add app loader

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,16 @@
+import {useAppContext} from "lms-core";
+import {lazy, useMemo} from "react";
+
 export function asset(path: string) {
     return new URL(path, import.meta.url).href;
+}
+
+export function useAppLoader(name: string) {
+    const context = useAppContext();
+    const loader = context?.appLoader(name);
+
+    return useMemo(
+        () => (loader ? lazy(loader) : null),
+        [loader]
+    );
 }
